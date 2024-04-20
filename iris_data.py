198053da0469e7cc6d260a63906dafcf3cef53c2
safetybabyboy <66130301@dpu.ac.th>
2024-04-20 18:58:26 +0700

    update

--- iris_data.py
+++ iris_data.py
@@ -1,4 +1,4 @@
-from sklearn.datasets import load_iris
+'''from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
@@ -21,4 +21,26 @@ def predict_iris(features):
     X, y = load_data()
     model = train_model(X, y)
     predicted_iris_type = model.predict([features])[0]
+    return 'setosa' if predicted_iris_type == 0 else ('versicolor' if predicted_iris_type == 1 else 'virginica')'''
+
+from flask import Flask
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+
+# Load Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create Decision Tree Classifier
+clf = DecisionTreeClassifier()
+clf.fit(X, y)
+
+def predict_iris(features):
+    predicted_iris_type = clf.predict([features])[0]
     return 'setosa' if predicted_iris_type == 0 else ('versicolor' if predicted_iris_type == 1 else 'virginica')
--- webapp.py
+++ webapp.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
 from iris_data import predict_iris
+import numpy as np
+import psycopg2
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', prediction=None)
 
@@ -19,7 +22,24 @@ def predict():
     features = [sepal_length, sepal_width, petal_length, petal_width]
     prediction = predict_iris(features)
 
-    return render_template('index.html', prediction=prediction)
+    conn = psycopg2.connect(host='localhost', port='5432', user='postgres', password='admin', database='iris')
+    cur = conn.cursor()
+    #cur.execute('CREATE TABLE allresult (sepal_length float, sepal_width float, petal_length float, petal_width float, prediction varchar);')
+    cur.execute('INSERT INTO allresult (sepal_length, sepal_width, petal_length, petal_width, prediction) values (%s, %s, %s, %s, %s)', (sepal_length, sepal_width, petal_length, petal_width, prediction))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('prediction.html', prediction=prediction)
+
+@app.route('/all_result', methods=['GET'])
+def all_result():
+    conn = psycopg2.connect(host='localhost', port='5432', user='postgres', password='admin', database='iris')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM public.allresult")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('all_result.html', rows=rows)
 
 
 if __name__ == '__main__':
